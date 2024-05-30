c258d310c077a874b3056bdb9487c96f748cf849
devhamim <hamim735@gmail.com>
2024-05-30 12:57:06 +0600

    update

--- app/Http/Middleware/VisitorCounter.php
+++ app/Http/Middleware/VisitorCounter.php
@@ -20,13 +20,42 @@ class VisitorCounter
     public function handle(Request $request, Closure $next): Response
     {
         $ip = $request->ip();
-        $location = Location::get($ip);
+        if ($request->hasHeader('X-Forwarded-For')) {
+            $ip = $request->header('X-Forwarded-For');
+        } elseif ($request->hasHeader('CF-Connecting-IP')) {
+            $ip = $request->header('CF-Connecting-IP');
+        }
 
-        Visitor::create([
-            'ip' => $ip,
-            'country' => $location ? $location->countryName : 'Unknown',
-            'city' => $location ? $location->cityName : 'Unknown',
-        ]);
+        $existingVisitor = Visitor::where('ip', $ip)
+            ->where('created_at', '>=', Carbon::now()->subDay())
+            ->first();
+
+        if (!$existingVisitor) {
+            if ($ip == '127.0.0.1' || $ip == '::1') {
+                $location = (object)[
+                    'countryName' => 'Localhost',
+                    'cityName' => 'Localhost',
+                ];
+            } else {
+                $location = Location::get($ip);
+            }
+
+            if (!$location) {
+                $location = (object)[
+                    'countryName' => config('location.position.default.countryName'),
+                    'cityName' => config('location.position.default.cityName'),
+                ];
+            }
+
+            Log::info('Visitor IP: ' . $ip);
+            Log::info('Location Data: ', (array)$location);
+
+            Visitor::create([
+                'ip' => $ip,
+                'country' => $location->countryName,
+                'city' => $location->cityName,
+            ]);
+        }
 
         return $next($request);
     }
