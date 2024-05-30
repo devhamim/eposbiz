03688a428cf2ec589ce318a7dcd7a50170b2221c
devhamim <hamim735@gmail.com>
2024-05-30 12:39:12 +0600

    vi

--- app/Http/Middleware/VisitorCounter.php
+++ app/Http/Middleware/VisitorCounter.php
@@ -19,29 +19,13 @@ class VisitorCounter
     public function handle(Request $request, Closure $next): Response
     {
         $ip = $request->ip();
+        $location = Location::get($ip);
 
-        // Check if a record for this IP exists within the last 24 hours
-        $existingVisitor = Visitor::where('ip', $ip)
-            ->where('created_at', '>=', Carbon::now()->subDay())
-            ->first();
-
-        if (!$existingVisitor) {
-            // Handle localhost IPs
-            if ($ip == '127.0.0.1' || $ip == '::1') {
-                $location = (object)[
-                    'countryName' => 'Localhost',
-                    'cityName' => 'Localhost',
-                ];
-            } else {
-                $location = Location::get($ip);
-            }
-
-            Visitor::create([
-                'ip' => $ip,
-                'country' => $location ? $location->countryName : 'Unknown',
-                'city' => $location ? $location->cityName : 'Unknown',
-            ]);
-        }
+        Visitor::create([
+            'ip' => $ip,
+            'country' => $location ? $location->countryName : 'Unknown',
+            'city' => $location ? $location->cityName : 'Unknown',
+        ]);
 
         return $next($request);
     }
